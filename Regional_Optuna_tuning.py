07a18b1890c61bcf8194fcf80ff189b5d19cff4b
Cristobal Mendoza <cristobal.mendoza@cea.fr>
2026-03-13 07:51:33 +0100

    Details

--- Regional_Optuna_tuning.py
+++ Regional_Optuna_tuning.py
@@ -55,6 +55,11 @@ from concurrent.futures import ProcessPoolExecutor as Pool
 from optuna.storages import JournalStorage
 from optuna.storages.journal import JournalFileBackend
 
+def print_best_trial(study, trial):
+    print(
+        f"{bcolors.CYAN}Trial {trial.number} value: {trial.value} | "
+        f"Best value so far: {study.best_value} | "
+        f"Best params: {study.best_params}{bcolors.RESET}")
 
 def is_range(x):
     return isinstance(x, (list, ListConfig))
@@ -193,7 +198,7 @@ def Run_optuna_optimization(config):
             #pruner = MedianPruner(n_startup_trials=5, n_warmup_steps=5, interval_steps=1)
             #pruner = PatientPruner(MedianPruner(n_startup_trials=5, n_warmup_steps=5, interval_steps=1), patience=1, min_delta  = 100)
             #pruner = MedianPruner(n_startup_trials=5, n_warmup_steps=5, interval_steps=1)
-            pruner = PatientPruner(wrapped_pruner=None, patience=3, min_delta  = 100)
+            pruner = PatientPruner(wrapped_pruner=None, patience=3, min_delta  = 200)
 
             sampler = optuna.samplers.TPESampler()
             study = optuna.create_study(study_name=study_name,directions=['minimize'],
@@ -202,11 +207,11 @@ def Run_optuna_optimization(config):
         else:
             print(f'{bcolors.YELLOW}Maximizing AUC{bcolors.RESET}')
             sampler = optuna.samplers.NSGAIISampler()
-            study = optuna.create_study(study_name=study_name,directions=['minimize','maximize'],
+            study = optuna.create_study(study_name=study_name,directions=['maximize'],
                                         storage=storage_name,sampler=sampler,
                                         load_if_exists=True)
         
-        study.optimize(lambda trial: objective(trial,config), n_trials=config.optuna_ntrials)
+        study.optimize(lambda trial: objective(trial,config), n_trials=config.optuna_ntrials, callbacks=[print_best_trial])
 
     except optuna.TrialPruned:
         pass
