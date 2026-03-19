e25f70bfce2cf589905e85803af9c808d33bd2f4
Cristobal Mendoza <cristobal.mendoza@cea.fr>
2026-03-19 16:55:43 +0100

    Details

--- Regional_Optuna_tuning.py
+++ Regional_Optuna_tuning.py
@@ -200,7 +200,7 @@ def Run_optuna_optimization(config):
 
         if config.Anomaly == None:
             print(f'{bcolors.YELLOW}Minimizing Reconstruction Error{bcolors.RESET}')
-            pruner = MedianPruner(n_startup_trials=5, n_warmup_steps=5, interval_steps=1)
+            pruner = MedianPruner(n_startup_trials=5, n_warmup_steps=4, interval_steps=1)
             sampler = optuna.samplers.TPESampler(n_startup_trials=5)
             study = optuna.create_study(study_name=study_name,directions=['minimize'],
                                         storage=storage_name,sampler=sampler,
